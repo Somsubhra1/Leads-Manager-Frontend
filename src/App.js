6b5d0a68c0742f9965db1a06395d572c9e854290
Somsubhra Das <das.somsubhra1@gmail.com>
2021-05-30 13:40:17 +0530

    components done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 // import "./App.css";
+import { Fragment } from "react";
+import Dashboard from "./components/leads/Dashboard";
 import Header from "./layout/Header";
 
 function App() {
-  return <Header />;
+  return (
+    <Fragment>
+      <Header />
+      <div className="container">
+        <Dashboard />
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
