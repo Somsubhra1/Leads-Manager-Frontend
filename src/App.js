badaabd6bb245089c619b8394e29b4e7520eb37b
Somsubhra Das <das.somsubhra1@gmail.com>
2021-06-03 11:57:30 +0530

    routers created with login and register components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // import "./App.css";
 import { Fragment } from "react";
 import Dashboard from "./components/leads/Dashboard";
+import Register from "./components/accounts/Register";
+import Login from "./components/accounts/Login";
 import Header from "./layout/Header";
 import store from "./store";
 import { Provider } from "react-redux";
@@ -8,6 +10,13 @@ import { Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
 import React, { Component } from "react";
 import Alerts from "./layout/Alerts";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
 const alertOptions = {
   timeout: 3000,
@@ -19,13 +28,19 @@ export class App extends Component {
     return (
       <Provider store={store}>
         <AlertProvider template={AlertTemplate} {...alertOptions}>
-          <Fragment>
-            <Header />
-            <Alerts />
-            <div className="container">
-              <Dashboard />
-            </div>
-          </Fragment>
+          <Router>
+            <Fragment>
+              <Header />
+              <Alerts />
+              <div className="container">
+                <Switch>
+                  <Route exact path="/" component={Dashboard} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
         </AlertProvider>
       </Provider>
     );
