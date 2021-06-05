613e81b1bcf1ddaf4be56602b8b9737a88886902
Somsubhra Das <das.somsubhra1@gmail.com>
2021-06-05 13:32:24 +0530

    added authentication token to leads

--- src/actions/leads.js
+++ src/actions/leads.js
@@ -1,13 +1,14 @@
 import axios from "../axios";
+import { tokenConfig } from "./auth";
 import { createMessage, returnErrors } from "./messages";
 
 import { DELETE_LEAD, GET_LEADS, ADD_LEAD, GET_ERRORS } from "./types";
 
 // GET LEADS
 
-export const getLeads = () => (dispatch) => {
+export const getLeads = () => (dispatch, getState) => {
   axios
-    .get("/api/leads/")
+    .get("/api/leads/", tokenConfig(getState))
     .then((res) => {
       dispatch({ type: GET_LEADS, payload: res.data });
     })
@@ -18,9 +19,9 @@ export const getLeads = () => (dispatch) => {
 
 // DELETE LEAD
 
-export const deleteLead = (id) => (dispatch) => {
+export const deleteLead = (id) => (dispatch, getState) => {
   axios
-    .delete(`/api/leads/${id}/`)
+    .delete(`/api/leads/${id}`, tokenConfig(getState))
     .then((res) => {
       dispatch(createMessage({ deleteLead: "Lead Deleted" }));
       dispatch({ type: DELETE_LEAD, payload: id });
@@ -30,9 +31,9 @@ export const deleteLead = (id) => (dispatch) => {
 
 // ADD LEAD
 
-export const addLead = (lead) => (dispatch) => {
+export const addLead = (lead) => (dispatch, getState) => {
   axios
-    .post(`/api/leads/`, lead)
+    .post(`/api/leads/`, lead, tokenConfig(getState))
     .then((res) => {
       dispatch(createMessage({ addLead: "Lead Added" }));
 
